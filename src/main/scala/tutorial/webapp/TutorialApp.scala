b254f3ad6c380f5d03fca34b491e909912137696
Divan Visagie <visagiedivan@gmail.com>
2016-06-21 21:34:27 +0200

    Update require location

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -5,10 +5,12 @@ import scala.scalajs.js
 import js.DynamicImplicits._
 import js.Dynamic.{global => g}
 
+val console = g.require("better-console")
+
 object TutorialApp extends JSApp {
 
   def main(): Unit = {
-    val console = g.require("better-console")
+
     console.error("oops")
     println("Hello world!")
   }
